0c5d0357df1dddb729ef2d35545a995e7aa1395a
Junyoung Lee <junily@yonsei.ac.kr>
2025-11-11 01:24:42 +0900

    chore: 초기 환경 세팅

---
+++ src/main/java/vote/vote_be/VoteBeApplication.java
@@ -0,0 +1,13 @@
+package vote.vote_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vote/vote_be/VoteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vote.vote_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
